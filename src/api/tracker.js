40a753ddbf84163ac8ceba6f09ba0ce46d8de29a
Ciri Ryan <cirijaye@yahoo.com>
2022-04-22 13:14:30 -0700

    add polyline, create trackContext add to app

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://d044-75-172-49-245.ngrok.io'
+    baseURL: 'http://6b51-75-172-49-245.ngrok.io'
 });
 
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,38 +1,42 @@
 import React, { useContext } from "react";
 import Spacer from "./Spacer";
 import { Input, Button } from "react-native-elements";
-import { Context as LocationContext } from '../context/LocationContext';
+import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackForm = () => {
-  const { state: { name, recording, locations }, 
-    startRecording, 
-    stopRecording, 
-    changeName 
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
   } = useContext(LocationContext);
-
-  console.log(locations.length)
 
   return (
     <>
       <Spacer />
       <Spacer>
-        <Input 
+        <Input
           value={name}
           onChangeText={changeName}
           placeholder="Enter Track Name"
         />
       </Spacer>
 
-      <Spacer />
-    {recording ? 
-    (
-      <Button title="Stop" onPress={stopRecording} /> 
-    ) : (
-     <Button title="Start Recording" onPress={startRecording} /> 
-     )}
+      <Spacer>
+      {recording ? (
+        <Button title="Stop" onPress={stopRecording} />
+      ) : (
+        <Button title="Start Recording" onPress={startRecording} />
+      )}
+      </Spacer>
+       
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title="Save Recording" />
+        ) : null}
+      </Spacer>
     </>
   );
 };
 
 export default TrackForm;
- 
